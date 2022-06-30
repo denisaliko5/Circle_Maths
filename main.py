5a65b00a524032ed7f4b58b94dd8e9cf0627975b
Denis Aliko <108418070+denisaliko5@users.noreply.github.com>
2022-06-30 15:22:16 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+import math
+
+#Variables
+radius = 5.5
+circumference = 2*radius*math.pi
+area = radius**2*math.pi
+print("The circumference is: %.2f" %(circumference) + " cm")
+print("The area is: %.2f" %(area) + " cm^2")
